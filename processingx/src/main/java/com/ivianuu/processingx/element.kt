e7bbdc86c4f9841059521e2e013d5af10204eba8
ivianuu <IVIanuu@gmail.com>
2018-11-22 15:47:29 +0100

    Added annotated annotations extension

--- processingx/src/main/java/com/ivianuu/processingx/element.kt
+++ processingx/src/main/java/com/ivianuu/processingx/element.kt
@@ -1,5 +1,6 @@
 package com.ivianuu.processingx
 
+import com.google.auto.common.AnnotationMirrors
 import com.google.auto.common.MoreElements
 import javax.lang.model.element.Element
 import javax.lang.model.element.PackageElement
@@ -11,18 +12,29 @@ inline fun <reified T : Annotation> Element.hasAnnotation() =
         hasAnnotation(T::class)
 
 fun Element.hasAnnotation(clazz: KClass<out Annotation>) =
-        MoreElements.isAnnotationPresent(this, clazz.java)
+    hasAnnotation(clazz.java)
+
+fun Element.hasAnnotation(clazz: Class<out Annotation>) =
+    MoreElements.isAnnotationPresent(this, clazz)
 
 fun Element.hasAnnotations(vararg classes: KClass<out Annotation>) =
-        classes.all { hasAnnotation(it) }
+    hasAnnotations(*classes.map { it.java }.toTypedArray())
+
+fun Element.hasAnnotations(vararg classes: Class<out Annotation>) =
+    classes.all { hasAnnotation(it) }
 
 fun Element.getAnnotationMirror(clazz: KClass<out Annotation>) =
-        MoreElements.getAnnotationMirror(this, clazz.java).get()
-            ?: throw IllegalArgumentException("no annotation of type $clazz found")
+    getAnnotationMirror(clazz.java)
 
-fun Element.getAnnotationMirrorOrNull(clazz: KClass<out Annotation>) = try {
+fun Element.getAnnotationMirror(clazz: Class<out Annotation>) =
+    MoreElements.getAnnotationMirror(this, clazz).get()
+        ?: throw IllegalArgumentException("no annotation of type $clazz found")
+
+fun Element.getAnnotationMirrorOrNull(clazz: KClass<out Annotation>) =
+    getAnnotationMirrorOrNull(clazz.java)
+
+fun Element.getAnnotationMirrorOrNull(clazz: Class<out Annotation>) = try {
     getAnnotationMirror(clazz)
-
 } catch (e: Exception) {
     null
 }
@@ -32,3 +44,12 @@ inline fun <reified T : Annotation> Element.getAnnotationMirror() =
 
 inline fun <reified T : Annotation> Element.getAnnotationMirrorOrNull() =
     getAnnotationMirrorOrNull(T::class)
+
+inline fun <reified T : Annotation> Element.getAnnotatedAnnotations() =
+    getAnnotatedAnnotations(T::class)
+
+fun Element.getAnnotatedAnnotations(clazz: KClass<out Annotation>) =
+    getAnnotatedAnnotations(clazz.java)
+
+fun Element.getAnnotatedAnnotations(clazz: Class<out Annotation>) =
+    AnnotationMirrors.getAnnotatedAnnotations(this, clazz)
